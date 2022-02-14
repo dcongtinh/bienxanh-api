d4af29d74c2b641b3f13e639ce75450508d4d520
Tinh Dao <dcongtinh@gmail.com>
2022-02-14 22:13:31 +0700

    update CORS

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+        ' GET, POST, PATCH, PUT, DELETE, OPTIONS'
     )
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'X-Requested-With, content-type, Authorization'
+        'Origin, X-Requested-With, X-Auth-Token, Content-Type, Authorization,'
     )
     res.setHeader('Access-Control-Allow-Credentials', true)
     next()
