7243bf7fe40d9f071baf016917ceb8c280922565
Tinh Dao <dcongtinh@gmail.com>
2022-02-15 19:57:17 +0700

    update CORS

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import {
 } from 'routes'
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 
 const PORT = process.env.PORT || 4000
 
@@ -42,25 +42,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(boom())
 // CORS
-// app.use((req, res, next) => {
-//     // if (process.env.NODE_ENV !== 'production') {
-//     //     res.setHeader('Access-Control-Allow-Origin', '*')
-//     // } else {
-//     //     res.setHeader('Access-Control-Allow-Origin', config.WEB_URL)
-//     // }
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//     )
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Authorization'
-//     )
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Content-Type', 'application/x-www-form-urlencoded')
-//     next()
-// })
+app.use((req, res, next) => {
+    // if (process.env.NODE_ENV !== 'production') {
+    //     res.setHeader('Access-Control-Allow-Origin', '*')
+    // } else {
+    //     res.setHeader('Access-Control-Allow-Origin', config.WEB_URL)
+    // }
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Authorization, Accept'
+    )
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Content-Type', 'application/x-www-form-urlencoded')
+    next()
+})
+
 // // app.use(function (req, res, next) {
 // //     res.header('Access-Control-Allow-Origin', '*')
 // //     res.header(
